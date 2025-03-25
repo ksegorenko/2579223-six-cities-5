bc80b31331f14d8b6b3112a3a6dbc5943470e0d0
Ksenia Egorenko <kseniaegorenko66949@gmail.com>
2025-03-25 16:16:36 +0700

    Добавлены моковые данные

---
+++ src/mocks/offers.ts
@@ -0,0 +1,96 @@
+const Offers = [
+  {
+    id: '6af6f711-c28d-4121-82cd-e0b462a27f00',
+    title: 'Beautiful & luxurious studio at great location',
+    type: 'apartment',
+    price: 120,
+    city: {
+      name: 'Amsterdam',
+      location: {
+        latitude: 52.35514938496378,
+        longitude: 4.673877537499948,
+        zoom: 8
+      }
+    },
+    location: {
+      latitude: 52.35514938496378,
+      longitude: 4.673877537499948,
+      zoom: 8
+    },
+    isFavorite: false,
+    isPremium: false,
+    'rating': 4,
+    'previewImage': 'https://url-to-image/image.png'
+  },
+  {
+    id: '6af6f711-c28d-4121-82cd-e0b462a27f01',
+    title: 'Beautiful & luxurious studio at great location',
+    type: 'penthouse',
+    price: 3000,
+    city: {
+      name: 'Paris',
+      location: {
+        latitude: 32.35514938496378,
+        longitude: 4.873877537499948,
+        zoom: 9
+      }
+    },
+    location: {
+      latitude: 32.35514938496378,
+      longitude: 4.873877537499948,
+      zoom: 9
+    },
+    isFavorite: true,
+    isPremium: true,
+    'rating': 5,
+    'previewImage': 'https://url-to-image/image.png'
+  },
+  {
+    id: '6af6f711-c28d-4121-82cd-e0b462a27f02',
+    title: 'Beautiful & luxurious studio at great location',
+    type: 'hostel',
+    price: 30,
+    city: {
+      name: 'Berlin',
+      location: {
+        latitude: 72.35514938496378,
+        longitude: 8.673877537499948,
+        zoom: 7
+      }
+    },
+    location: {
+      latitude: 72.35514938496378,
+      longitude: 8.673877537499948,
+      zoom: 7
+    },
+    isFavorite: false,
+    isPremium: false,
+    'rating': 3,
+    'previewImage': 'https://url-to-image/image.png'
+  },
+  {
+    id: '6af6f711-c28d-4121-82cd-e0b462a27f03',
+    title: 'Beautiful & luxurious studio at great location',
+    type: 'apartment',
+    price: 170,
+    city: {
+      name: 'Milan',
+      location: {
+        latitude: 92.35514938496378,
+        longitude: 4.673877537499948,
+        zoom: 8
+      }
+    },
+    location: {
+      latitude: 92.35514938496378,
+      longitude: 4.673877537499948,
+      zoom: 8
+    },
+    isFavorite: true,
+    isPremium: false,
+    'rating': 5,
+    'previewImage': 'https://url-to-image/image.png'
+  },
+];
+
+export {Offers};
