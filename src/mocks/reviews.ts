3515e4292f9ede479b0cceb0c5cfff9f71b1ea27
Ksenia Egorenko <kseniaegorenko66949@gmail.com>
2025-03-26 16:35:10 +0700

    Добавлены моковые данные для отзывов

---
+++ src/mocks/reviews.ts
@@ -0,0 +1,48 @@
+const Reviews = [
+  {
+    id: 'b67ddfd5-b953-4a30-8c8d-bd083cd6b62a',
+    date: '2019-05-08T14:13:56.569Z',
+    user: {
+      name: 'Oliver Conner',
+      avatarUrl: 'https://url-to-image/image.png',
+      isPro: false
+    },
+    comment: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    rating: 4
+  },
+  {
+    id: 'b67ddfd5-b953-4a30-8c8d-bd083cd6b621',
+    date: '2022-06-08T14:13:56.569Z',
+    user: {
+      name: 'Kate Oliver',
+      avatarUrl: 'https://url-to-image/image.png',
+      isPro: true
+    },
+    comment: 'Cool place. Liked it.',
+    rating: 5
+  },
+  {
+    id: 'b67ddfd5-b953-4a30-8c8d-bd083cd6b622',
+    date: '2018-07-10T14:13:56.569Z',
+    user: {
+      name: 'Sasha Osipov',
+      avatarUrl: 'https://url-to-image/image.png',
+      isPro: false
+    },
+    comment: 'Not bad.',
+    rating: 3
+  },
+  {
+    id: 'b67ddfd5-b953-4a30-8c8d-bd083cd6b623',
+    date: '2019-05-08T14:13:56.569Z',
+    user: {
+      name: 'Helen Lecly',
+      avatarUrl: 'https://url-to-image/image.png',
+      isPro: false
+    },
+    comment: '',
+    rating: 4
+  },
+];
+
+export { Reviews };
