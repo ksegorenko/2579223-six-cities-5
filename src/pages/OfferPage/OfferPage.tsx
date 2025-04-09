5b14414467a9121f1aa21a61e4770bcd801c5615
Ksenia Egorenko <kseniaegorenko66949@gmail.com>
2025-04-09 11:07:07 +0700

    Добавлен компонент формы отправки комментария

--- src/pages/OfferPage/OfferPage.tsx
+++ src/pages/OfferPage/OfferPage.tsx
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom';
 import { Paths } from '../../const';
 import { getRatingWidth } from '../../utils';
 import PlaceCard from '../../components/PlaceCard/PlaceCard';
+import CommentForm from '../../components/CommentForm/CommentForm';
 
 type OfferPageProps = {
   offers: Offer[];
@@ -157,51 +158,7 @@ function OfferPage({offers}: OfferPageProps): JSX.Element {
                     </div>
                   </li>
                 </ul>
-                <form className="reviews__form form" action="#" method="post" />
-                <label className="reviews__label form__label" htmlFor="review">Your review</label>
-                <div className="reviews__rating-form form__rating">
-                  <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio" />
-                  <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
-                    <svg className="form__star-image" width="37" height="33">
-                      <use xlinkHref="#icon-star"></use>
-                    </svg>
-                  </label>
-
-                  <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio" />
-                  <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
-                    <svg className="form__star-image" width="37" height="33">
-                      <use xlinkHref="#icon-star"></use>
-                    </svg>
-                  </label>
-
-                  <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio" />
-                  <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-                    <svg className="form__star-image" width="37" height="33">
-                      <use xlinkHref="#icon-star"></use>
-                    </svg>
-                  </label>
-
-                  <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio" />
-                  <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
-                    <svg className="form__star-image" width="37" height="33">
-                      <use xlinkHref="#icon-star"></use>
-                    </svg>
-                  </label>
-
-                  <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio" />
-                  <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
-                    <svg className="form__star-image" width="37" height="33">
-                      <use xlinkHref="#icon-star"></use>
-                    </svg>
-                  </label>
-                </div>
-                <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved"></textarea>
-                <div className="reviews__button-wrapper">
-                  <p className="reviews__help">
-                  To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
-                  </p>
-                  <button className="reviews__submit form__submit button" type="submit" disabled>Submit</button>
-                </div>
+                <CommentForm />
               </div>
             </div>
           </section>
--- src/utils.ts
+++ src/utils.ts
@@ -3,4 +3,15 @@ function getRatingWidth(rating: number): string {
   return `${result}%`;
 }
 
-export { getRatingWidth };
+function getStarTitle(star: number): string {
+  switch (star) {
+    case 5: return 'perfect';
+    case 4: return 'good';
+    case 3: return 'not bad';
+    case 2: return 'badly';
+    case 1: return 'terribly';
+    default: return '';
+  }
+}
+
+export { getRatingWidth, getStarTitle };
